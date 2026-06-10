fffe9c0440410ef3e908cfcaee0fca676996820b
m3iyu <yekaterina.khokhlova.2007@list.ru>
2026-06-10 22:52:45 +0300

    Create Task2.java

---
+++ task-1/Task2.java
@@ -0,0 +1,47 @@
+public class Task2 {
+    static byte defByte;        
+    static short defShort;      
+    static int defInt;          
+    static long defLong;        
+    static float defFloat;      
+    static double defDouble;    
+    static char defChar;        
+    static boolean defBoolean;  
+
+    public static void main(String[] args) {
+        
+        byte a = 120;
+        short b = 1000;
+        int i = 100000;
+        long l = 1000000000L;
+        float f = 2.71f;
+        double d = 3.14;
+        char c = 'H';
+
+        
+        System.out.println(" Имя: \u041A\u0430\u0442\u044F");
+
+        
+        System.out.println("abc" + 3f);
+        System.out.println("300" + 0);
+        System.out.println("sadsv" + " dfsfs");
+        boolean tr = true;
+        System.out.println("Bool " + tr);
+
+
+        System.out.println(100 + 3.14); 
+        byte g = 100;
+        System.out.println(g + 10L); 
+
+        int z = 10;
+        byte r = (byte)z;
+
+        double db = 3.14;
+        float ds = (float)3.14;
+
+
+        var hello = "Hello World!"; 
+        var doub = 4.4234; 
+        var integ = 52; 
+    }
+}
