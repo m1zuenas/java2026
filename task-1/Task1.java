f77b3050d36d8db04439085e088ae95403431dcf
m3iyu <yekaterina.khokhlova.2007@list.ru>
2026-06-10 22:40:31 +0300

    Create Task1.java

---
+++ task-1/Task1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+
+        System.out.println("Привет, " + name);
+
+
+        int a = 3;
+        int b = 4;
+        System.out.println(a+b);
+    }
+}
